2edaad754edcd2eac0fa67fb9e8c14bacfa731de
peterPan1993 <123504543@qq.com>
2018-04-13 21:38:24 +0800

    create entity

---
+++ src/com/bdqn/entity/News.java
@@ -0,0 +1,12 @@
+package com.bdqn.entity;
+
+import java.io.Serializable;
+
+public class News implements Serializable{
+
+    private int nid;
+    private int ntid;
+    private int ntitle;
+
+
+}
